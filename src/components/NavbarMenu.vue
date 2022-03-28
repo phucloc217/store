2adcd1b8b1108d68bb2d1d1158933824a99a8489
Nguyễn Phúc Lộc <loc.nguyen456789@gmail.com>
2022-03-28 17:21:57 +0700

    Finish Products Page

--- src/components/NavbarMenu.vue
+++ src/components/NavbarMenu.vue
@@ -8,7 +8,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Trang chủ</a>
+            <router-link to="/" class="nav-link active"  aria-current="page">Trang chủ</router-link>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Giới thiệu</a>
@@ -25,11 +25,14 @@
           </ul>
         </li>
         <li class="nav-item dropdown me-5">
+          
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Sản phẩm
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li>
+                 <router-link to="/products" class="dropdown-item " id="student_menu">Ghế</router-link>
+              </li>
             <li><a class="dropdown-item" href="#">Another action</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,13 +1,303 @@
 <template>
-  
+  <div class="p-5" id="products">
+    <h1 class="text-light text-center fw-bold">SẢN PHẨM</h1>
+    <div class="row mt-5 d-flex flex-row-reverse">
+      <div class="col-md-2 col-sm-6 mt-2">
+        <div class="row">
+          <div class="row text-light fw-bold">Chất liệu</div>
+          <div class="row mt-2">
+            <select name="" id="" class="form-select bg-transparent text-light rounded-0 sort">
+              <option value="1" class="text-dark">Tất cả</option>
+              <option value="2" class="text-dark">Da</option>
+              <option value="2" class="text-dark">Vải</option>
+              <option value="2" class="text-dark">Gỗ</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-2 col-sm-6 mt-2">
+        <div class="row">
+          <div class="row text-light fw-bold">Giá</div>
+          <div class="row mt-2">
+            <select name="" id="" class="form-select bg-transparent text-light rounded-0 sort">
+              <option value="1" class="text-dark">Thấp đến cao</option>
+              <option value="2" class="text-dark">Cao đến thấp</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="list-product" class="mt-5 pt-2">
+      <div class="row pe-3">
+        <div class="col-md-3 col-sm-3">
+          <div class="card shadow p-0 m-2 bg-body rounded" style="width: 18rem">
+            <img
+              src="../assets/img/screen_2x.webp"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center mb-3">Ghế vintage Eames</h5>
+              <p class="card-text">
+                Một chiếc ghế vintage phù hợp cho những không gian đầy hoài
+                niệm.
+              </p>
+              <p class="card-text fw-bold text-center">1.000.000 đ</p>
+              <p></p>
+              <div class="d-flex justify-content-center">
+                <a href="#" class="btn btn-primary">Thêm vào giỏ hàng</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3">
+          <div class="card shadow p-0 m-2 bg-body rounded" style="width: 18rem">
+            <img
+              src="../assets/img/screen_2x.webp"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center mb-3">Ghế vintage Eames</h5>
+              <p class="card-text">
+                Một chiếc ghế vintage phù hợp cho những không gian đầy hoài
+                niệm.
+              </p>
+              <p class="card-text fw-bold text-center">1.000.000 đ</p>
+              <p></p>
+              <div class="d-flex justify-content-center">
+                <a href="#" class="btn btn-primary">Thêm vào giỏ hàng</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3">
+          <div class="card shadow p-0 m-2 bg-body rounded" style="width: 18rem">
+            <img
+              src="../assets/img/screen_2x.webp"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center mb-3">Ghế vintage Eames</h5>
+              <p class="card-text">
+                Một chiếc ghế vintage phù hợp cho những không gian đầy hoài
+                niệm.
+              </p>
+              <p class="card-text fw-bold text-center">1.000.000 đ</p>
+              <p></p>
+              <div class="d-flex justify-content-center">
+                <a href="#" class="btn btn-primary">Thêm vào giỏ hàng</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3">
+          <div class="card shadow p-0 m-2 bg-body rounded" style="width: 18rem">
+            <img
+              src="../assets/img/screen_2x.webp"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center mb-3">Ghế vintage Eames</h5>
+              <p class="card-text">
+                Một chiếc ghế vintage phù hợp cho những không gian đầy hoài
+                niệm.
+              </p>
+              <p class="card-text fw-bold text-center">1.000.000 đ</p>
+              <p></p>
+              <div class="d-flex justify-content-center">
+                <a href="#" class="btn btn-primary">Thêm vào giỏ hàng</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3">
+          <div class="card shadow p-0 m-2 bg-body rounded" style="width: 18rem">
+            <img
+              src="../assets/img/screen_2x.webp"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center mb-3">Ghế vintage Eames</h5>
+              <p class="card-text">
+                Một chiếc ghế vintage phù hợp cho những không gian đầy hoài
+                niệm.
+              </p>
+              <p class="card-text fw-bold text-center">1.000.000 đ</p>
+              <p></p>
+              <div class="d-flex justify-content-center">
+                <a href="#" class="btn btn-primary">Thêm vào giỏ hàng</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3">
+          <div class="card shadow p-0 m-2 bg-body rounded" style="width: 18rem">
+            <img
+              src="../assets/img/screen_2x.webp"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center mb-3">Ghế vintage Eames</h5>
+              <p class="card-text">
+                Một chiếc ghế vintage phù hợp cho những không gian đầy hoài
+                niệm.
+              </p>
+              <p class="card-text fw-bold text-center">1.000.000 đ</p>
+              <p></p>
+              <div class="d-flex justify-content-center">
+                <a href="#" class="btn btn-primary">Thêm vào giỏ hàng</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <div class="card shadow p-0 m-2 bg-body rounded" style="width: 18rem">
+            <img
+              src="../assets/img/screen_2x.webp"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center mb-3">Ghế vintage Eames</h5>
+              <p class="card-text">
+                Một chiếc ghế vintage phù hợp cho những không gian đầy hoài
+                niệm.
+              </p>
+              <p class="card-text fw-bold text-center">1.000.000 đ</p>
+              <p></p>
+              <div class="d-flex justify-content-center">
+                <a href="#" class="btn btn-primary">Thêm vào giỏ hàng</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3">
+          <div class="card shadow p-0 m-2 bg-body rounded" style="width: 18rem">
+            <img
+              src="../assets/img/screen_2x.webp"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center mb-3">Ghế vintage Eames</h5>
+              <p class="card-text">
+                Một chiếc ghế vintage phù hợp cho những không gian đầy hoài
+                niệm.
+              </p>
+              <p class="card-text fw-bold text-center">1.000.000 đ</p>
+              <p></p>
+              <div class="d-flex justify-content-center">
+                <a href="#" class="btn btn-primary">Thêm vào giỏ hàng</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3">
+          <div class="card shadow p-0 m-2 bg-body rounded" style="width: 18rem">
+            <img
+              src="../assets/img/screen_2x.webp"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center mb-3">Ghế vintage Eames</h5>
+              <p class="card-text">
+                Một chiếc ghế vintage phù hợp cho những không gian đầy hoài
+                niệm.
+              </p>
+              <p class="card-text fw-bold text-center">1.000.000 đ</p>
+              <p></p>
+              <div class="d-flex justify-content-center">
+                <a href="#" class="btn btn-primary">Thêm vào giỏ hàng</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3">
+          <div class="card shadow p-0 m-2 bg-body rounded" style="width: 18rem">
+            <img
+              src="../assets/img/screen_2x.webp"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center mb-3">Ghế vintage Eames</h5>
+              <p class="card-text">
+                Một chiếc ghế vintage phù hợp cho những không gian đầy hoài
+                niệm.
+              </p>
+              <p class="card-text fw-bold text-center">1.000.000 đ</p>
+              <p></p>
+              <div class="d-flex justify-content-center">
+                <a href="#" class="btn btn-primary">Thêm vào giỏ hàng</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3">
+          <div class="card shadow p-0 m-2 bg-body rounded" style="width: 18rem">
+            <img
+              src="../assets/img/screen_2x.webp"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center mb-3">Ghế vintage Eames</h5>
+              <p class="card-text">
+                Một chiếc ghế vintage phù hợp cho những không gian đầy hoài
+                niệm.
+              </p>
+              <p class="card-text fw-bold text-center">1.000.000 đ</p>
+              <p></p>
+              <div class="d-flex justify-content-center">
+                <a href="#" class="btn btn-primary">Thêm vào giỏ hàng</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3">
+          <div class="card shadow p-0 m-2 bg-body rounded" style="width: 18rem">
+            <img
+              src="../assets/img/screen_2x.webp"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center mb-3">Ghế vintage Eames</h5>
+              <p class="card-text">
+                Một chiếc ghế vintage phù hợp cho những không gian đầy hoài
+                niệm.
+              </p>
+              <p class="card-text fw-bold text-center">1.000.000 đ</p>
+              <p></p>
+              <div class="d-flex justify-content-center">
+                <a href="#" class="btn btn-primary">Thêm vào giỏ hàng</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped>
+#products {
+  background-color: #a68b6b;
+}
+.sort{
+    border-top: 0;
+    border-left: 0;
+    border-right: 0;
+    border-bottom: 2;
+}
 </style>
